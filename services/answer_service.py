36ca91b6abffbfb7c5c29f8fcb2687035595b2ec
Sayed Nowshad <23691A32A2@mits.ac.in>
2026-06-15 22:40:51 +0530

    phase-15-completed-all-layers-done-hurrah

--- services/answer_service.py
+++ services/answer_service.py
@@ -6,9 +6,9 @@ from services.rerank_service import (
     RerankService
 )
 
-from services.graph_retrieval_service import (
-    GraphRetrievalService
-)
+# from services.graph_retrieval_service import (
+#     GraphRetrievalService
+# )
 
 from llm.llm_client import (
     LLMClient
@@ -50,6 +50,15 @@ from memory.memory_deduplicator import (
     MemoryDeduplicator
 )
 
+from services.evidence_extractor import (
+    EvidenceExtractor
+)
+
+import time
+
+from services.analytics_service import (
+    AnalyticsService
+)
 class AnswerService:
 
     @staticmethod
@@ -57,55 +66,71 @@ class AnswerService:
         question: str,
         top_k: int = 5
     ):
-
+        start_time = time.time()
+        
         conversation_context = (
             ConversationManager
             .get_context()
         )
-
+        
+        import re
+        
         pronouns = [
             "it",
+            "its",
             "this",
             "that",
             "these",
-            "those"
+            "those",
+            "they",
+            "them",
+            "their"
         ]
 
         original_question = question
 
         current_topic = None
-
+        
+        question_words = re.findall(
+            r"\b\w+\b",
+            question.lower()
+        )
+        
+        print(
+            "Question Words:",
+             question_words
+        )
+        
         if any(
-            word in question.lower()
-            for word in pronouns
+            word in pronouns
+            for word in question_words
         ):
 
-           current_topic = (
+            current_topic = (
                 ConversationManager
                 .get_current_topic()
             )
 
-           if not current_topic:
+            if not current_topic:
 
-                                    return {
-            "question": original_question,
-            "answer":
-            (
-                "I need more context. "
-                "What does 'it' refer to?"
-            ),
-            "sources": []
+                return {
+                    "question": original_question,
+                    "answer":
+                (
+                    "I need more context. "
+                    "What does 'it' refer to?"
+                ),
+                "sources": []
         }
+            question = (
+                    f"{question} "
+                    f"regarding "
+                    f"{current_topic}"
+                    )
 
-        question = (
-        f"{question} "
-        f"regarding "
-        f"{current_topic}"
-        )
-
-        print(
-        "Enhanced Question:",
-        question
+            print(
+                "Enhanced Question:",
+                question
         )
         
         # wait....
@@ -116,6 +141,8 @@ class AnswerService:
             )
         )
 
+        retrieval_start = time.time()
+        
         all_chunks = []
 
         for query in queries:
@@ -134,7 +161,7 @@ Current Question:
                 HybridRetrievalService
                 .search(
                     query=enhanced_query,
-                    top_k=5
+                    top_k=top_k
                 )
             )
 
@@ -153,11 +180,18 @@ Current Question:
         chunks = list(
             unique_chunks.values()
         )
+        retrieval_time = (
+            time.time()
+            - retrieval_start
+        )
 
         print(
             f"\nRetrieved Chunks: {len(chunks)}"
         )
-
+        
+        # added timing for retrieval and reranking
+        rerank_start = time.time()
+        
         chunks = (
             RerankService
             .rerank(
@@ -170,6 +204,11 @@ Current Question:
         print(
             "\nFINAL CHUNKS AFTER RERANK:\n"
         )
+        
+        rerank_time = (
+            time.time()
+            - rerank_start
+        )
 
         for chunk in chunks:
 
@@ -178,6 +217,10 @@ Current Question:
             )
 
         if not chunks:
+
+            AnalyticsService.print_failure(
+                "NO_CHUNKS"
+            )
 
             return {
                 "question": original_question,
@@ -209,15 +252,9 @@ Current Question:
             )
         )
 
-        context_parts = []
-
         sources = set()
 
         for chunk in chunks:
-
-            context_parts.append(
-                chunk["content"]
-            )
 
             sources.add(
                 chunk["source"]
@@ -227,8 +264,18 @@ Current Question:
             related_concepts
         )
 
-        document_context = "\n\n".join(
-            context_parts
+        document_context = (
+            EvidenceExtractor.extract(
+                chunks=chunks,
+                question=question,
+                max_sentences=10
+    )
+)
+        print(
+            "\nCOMPRESSED CONTEXT:\n"
+)
+        print(
+             document_context
         )
 
         memory_context = "\n\n".join(
@@ -285,12 +332,32 @@ in the context, say so.
 
         print("\nPROMPT:\n")
         print(prompt[:4000])
+        
+        llm_start = time.time()
 
         answer = (
             LLMClient.generate(
                 prompt
             )
         )
+        
+        llm_time = (
+            time.time()
+            - llm_start
+        )
+        
+        if "LLM Error" in answer:
+
+            AnalyticsService.print_failure(
+            "LLM_ERROR"
+        )
+        if (
+            "I could not find"
+            in answer
+    ):
+            AnalyticsService.print_failure(
+            "NO_ANSWER"
+    )
 
         topic = (
             TopicExtractor
@@ -316,25 +383,87 @@ in the context, say so.
         )
 
         if (
-    "I could not find the answer"
-    not in answer
-    ):
+            "I could not find the answer"
+            not in answer
+        ):
 
-         if (
-            MemoryDeduplicator
-            .should_save(
-            original_question
+            if (
+                MemoryDeduplicator
+                .should_save(
+                    original_question
+                )
+            ):
+
+                MemoryManager.save_conversation(
+                    question=original_question,
+                    answer=answer
+                )
+
+            else:
+
+                print(
+                    "Duplicate memory skipped."
+                )
+
+        # ==========================
+        # ANALYTICS
+        # ==========================
+
+        total_time = (
+            time.time()
+            - start_time
         )
-    ):
 
-             MemoryManager.save_conversation(
-            question=original_question,
-            answer=answer
-        )
+        analytics = {
 
-        else:
-             print(
-            "Duplicate memory skipped."
+            "question":
+                original_question,
+
+            "queries":
+                len(queries),
+
+            "retrieved_chunks":
+                len(all_chunks),
+
+            "final_chunks":
+                len(chunks),
+
+            "memory_hits":
+                len(memories),
+
+            "sources_names":
+                list(sources),
+
+            "answer_length":
+                len(answer),
+
+            "retrieval_time":
+                round(
+                    retrieval_time,
+                    3
+                ),
+
+            "rerank_time":
+                round(
+                    rerank_time,
+                    3
+                ),
+
+            "llm_time":
+                round(
+                    llm_time,
+                    3
+                ),
+
+            "total_time":
+                round(
+                    total_time,
+                    3
+                )
+        }
+
+        AnalyticsService.print_dashboard(
+            analytics
         )
 
         return {
